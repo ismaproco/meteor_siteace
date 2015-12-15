b1353b8a238c2cda05e6ad61c4092336c6c68ca4
ismaproco@gmail.com <ismaproco@gmail.com>
2015-12-15 18:38:26 +0000

    adding website details template and routing

--- client/routes.js
+++ client/routes.js
@@ -2,3 +2,13 @@ Router.route('/', function () {
     this.layout('ApplicationLayout');
     this.render("summary",{to:'main'});
 });
+
+Router.route('/w/:_id', function () {
+    this.layout('ApplicationLayout');
+    this.render("detail", { 
+        to:'main', 
+        data: function(){
+            return Websites.findOne({ _id: this.params._id });
+        }
+    });
+});
