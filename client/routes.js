442894cd0877eaf2d4544dacd159e2322b2c5182
ismaproco@gmail.com <ismaproco@gmail.com>
2015-12-15 18:41:11 +0000

    fix template detail

--- client/routes.js
+++ client/routes.js
@@ -5,7 +5,7 @@ Router.route('/', function () {
 
 Router.route('/w/:_id', function () {
     this.layout('ApplicationLayout');
-    this.render("detail", { 
+    this.render("website_detail", { 
         to:'main', 
         data: function(){
             return Websites.findOne({ _id: this.params._id });
